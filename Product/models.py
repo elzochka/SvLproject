01e8ce9d513d86b6470305e0d511a167fa626a96
Alina <xvxcherryxvx@gmail.com>
2018-06-04 12:02:19 +0300

    .gitignore is now working

--- Product/models.py
+++ Product/models.py
@@ -1,3 +1,14 @@
+
+from datetime import datetime
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    images = models.ImageField(height_field=None, width_field=None, max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=5, default="UAH")
+    description = models.TextField()
+    date_of_publishing = models.DateField(blank=True, default=datetime.now)
+    date_of_adding = models.DateField(auto_now=True)
