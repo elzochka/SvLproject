3ea0d03f0421a14cc99e4bdc01ffd71c9d695f1e
Alina <xvxcherryxvx@gmail.com>
2018-06-05 10:09:25 +0300

    made changes in single_product.html file. connected static and transformed js html and img files into static

--- SvL/settings.py
+++ SvL/settings.py
@@ -117,8 +117,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'kotzilla/home/myvirtenv/bin/firstapp/static/',
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- SvL/urls.py
+++ SvL/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^single_product/$', single_product),
 
+
 ]
